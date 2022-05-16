bc1aa8969131f0f6732e94c9df96bd0910ff5f33
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-16 06:46:37 +0600

    Added Get Service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var cors = require('cors');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -11,6 +11,22 @@ require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eq9rh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try {
+    await client.connect();
+    const serviceCollection = client.db("care-aid").collection("services");
+
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+  }finally{}
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Care Aid Works Successfully')
